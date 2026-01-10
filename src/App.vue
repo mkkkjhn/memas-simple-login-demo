f0ad5b649919f6076e9cf23160be15e08f09f68e
artem.sidak <prihodovconnection@gmail.com>
2026-01-10 21:44:13 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped lang="scss">
+$primary-color: #4fc08d;
+$logo-margin: 2rem;
+
+header {
+  line-height: 1.5;
+
+  .logo {
+    display: block;
+    margin: 0 auto $logo-margin;
+  }
+
+  .wrapper {
+    display: flex;
+    place-items: center;
+  }
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+
+    .wrapper {
+      display: flex;
+      place-items: flex-start;
+      flex-wrap: wrap;
+    }
+  }
+}
+</style>

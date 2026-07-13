bfba3b6efca5b6daf326186fcd306f6ff1a210cc
artem.sidak <prihodovconnection@gmail.com>
2026-07-13 19:00:24 +0300

    chore: set Vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/memas-simple-login-demo/',
   plugins: [
     vue(),
     vueDevTools(),
